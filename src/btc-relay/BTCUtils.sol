6de7a442abc2e75a96d559d73682198d8080a473
Philippe Dumonet <philogy@Philippes-MacBook-Air.local>
2023-03-01 15:55:02 -0700

    Add witness proving and extracting code

--- src/btc-relay/BTCUtils.sol
+++ src/btc-relay/BTCUtils.sol
@@ -3,7 +3,11 @@ pragma solidity 0.8.15;
 
 /// @author philogy <https://github.com/philogy>
 library BTCUtils {
-    function isValidMerkle(bytes32 _txRoot, bytes32 _baseEl, uint256 _index, bytes32[] calldata _proofEls)
+    error NotSegwitTx();
+    error InsufficientTxInputs();
+    error InsufficientWitnessElements();
+
+    function isValidMerkle(bytes32 _root, bytes32 _baseEl, uint256 _index, bytes32[] calldata _proofEls)
         internal
         view
         returns (bool isValid)
@@ -24,7 +28,180 @@ library BTCUtils {
                 _index := shr(1, _index)
                 proofOffset := add(proofOffset, 0x20)
             }
-            isValid := eq(_txRoot, _baseEl)
+            isValid := eq(_root, _baseEl)
+        }
+    }
+
+    function getWitnessScript(bytes calldata _witnessTx, uint256 _inIndex)
+        internal
+        pure
+        returns (bytes memory witnessScript)
+    {
+        assembly {
+            function getVarint(ptr) -> newPtr, length {
+                let maxData := and(calldataload(add(ptr, 0x8)), 0xffffffffffffffffff)
+                let varLengthByte := shr(0x40, maxData)
+                newPtr := add(ptr, 1)
+                switch lt(varLengthByte, 0xfd)
+                case 1 { length := varLengthByte }
+                default {
+                    // 0xfd -> 8 * 6
+                    // 0xfe -> 8 * 4
+                    // 0xff -> 8 * 0
+                    // 8 * (8 - 2^(vlb - 0xfc)) = 64 - 8 * 2^(vlb - 0xfc)
+                    let size := shl(sub(varLengthByte, 0xfc), 1)
+                    newPtr := add(newPtr, size)
+                    let data := and(maxData, 0xffffffffffffffff)
+                    length := shl(sub(0x40, shl(3, size)), data)
+                }
+            }
+
+            function varintSkip(ptr) -> newPtr {
+                let maxData := and(calldataload(add(ptr, 0x8)), 0xffffffffffffffffff)
+                let varLengthByte := shr(0x40, maxData)
+                newPtr := add(ptr, 1)
+                switch lt(varLengthByte, 0xfd)
+                case 1 { newPtr := add(newPtr, varLengthByte) }
+                default {
+                    // 0xfd -> 8 * 6
+                    // 0xfe -> 8 * 4
+                    // 0xff -> 8 * 0
+                    // 8 * (8 - 2^(vlb - 0xfc)) = 64 - 8 * 2^(vlb - 0xfc)
+                    let size := shl(sub(varLengthByte, 0xfc), 1)
+                    newPtr := add(newPtr, size)
+                    let data := and(maxData, 0xffffffffffffffff)
+                    newPtr := add(shl(sub(0x40, shl(3, size)), data), newPtr)
+                }
+            }
+
+            // Start pointer 4 bytes after beginning, skipping version bytes
+            let ptr := sub(_witnessTx.offset, 0x1a)
+
+            // Check mark and flag bytes
+            if iszero(eq(and(calldataload(ptr), 0xffff), 0x0001)) {
+                mstore(0x00, 0xf39be9d7)
+                revert(0x1c, 0x04)
+            }
+            ptr := add(ptr, 1)
+
+            // Read and jump over tx inputs
+            let txinCount
+            ptr, txinCount := getVarint(ptr)
+            if iszero(lt(_inIndex, txinCount)) {
+                mstore(0x00, 0x03db5ed9)
+                revert(0x1c, 0x04)
+            }
+            for { let i := 0 } lt(i, txinCount) { i := add(i, 1) } {
+                ptr := add(ptr, 36)
+                ptr := varintSkip(ptr)
+                ptr := add(ptr, 4)
+            }
+
+            // Read and jump over tx outputs
+            let txoutCount
+            ptr, txoutCount := getVarint(ptr)
+            for { let i := 0 } lt(i, txoutCount) { i := add(i, 1) } {
+                ptr := add(ptr, 8)
+                ptr := varintSkip(ptr)
+            }
+
+            // Jump over unneeded witnesses.
+            let witnessEls
+            for { let i := 0 } lt(i, _inIndex) { i := add(i, 1) } {
+                ptr, witnessEls := getVarint(ptr)
+                for { let j := 0 } lt(j, witnessEls) { j := add(j, 1) } {
+                    // Skip actual witness element
+                    ptr := varintSkip(ptr)
+                }
+            }
+
+            // Get actual witness
+            ptr, witnessEls := getVarint(ptr)
+            // Checkt that there's at least two witnesses
+            if iszero(gt(witnessEls, 1)) {
+                mstore(0x00, 0x0d6fd37b)
+                revert(0x1c, 0x04)
+            }
+            let witnessesToSkip := sub(witnessEls, 2)
+            for { let i := 0 } lt(i, witnessesToSkip) { i := add(i, 1) } { ptr := varintSkip(ptr) }
+
+            witnessScript := mload(0x40)
+            let witnessLen
+            ptr, witnessLen := getVarint(ptr)
+            mstore(witnessScript, witnessLen)
+            let freeMem := add(witnessScript, 0x20)
+            calldatacopy(freeMem, add(ptr, 0x1f), witnessLen)
+            freeMem := and(add(freeMem, add(witnessLen, 0x1f)), 0xffffffffffffffe0)
+            mstore(0x40, freeMem)
+        }
+    }
+
+    /// @dev Will use all gas if transaction does not contain witness commitment.
+    /// @dev Expects tx without marker, flag or witness
+    function getWitnessRootFromCoinbase(bytes calldata _coinbaseTx) internal pure returns (bytes32 witnessRoot) {
+        assembly {
+            function getVarint(ptr) -> newPtr, length {
+                let maxData := and(calldataload(add(ptr, 0x8)), 0xffffffffffffffffff)
+                let varLengthByte := shr(0x40, maxData)
+                newPtr := add(ptr, 1)
+                switch lt(varLengthByte, 0xfd)
+                case 1 { length := varLengthByte }
+                default {
+                    // 0xfd -> 8 * 6
+                    // 0xfe -> 8 * 4
+                    // 0xff -> 8 * 0
+                    // 8 * (8 - 2^(vlb - 0xfc)) = 64 - 8 * 2^(vlb - 0xfc)
+                    let size := shl(sub(varLengthByte, 0xfc), 1)
+                    newPtr := add(newPtr, size)
+                    let data := and(maxData, 0xffffffffffffffff)
+                    length := shl(sub(0x40, shl(3, size)), data)
+                }
+            }
+
+            function varintSkip(ptr) -> newPtr {
+                let maxData := and(calldataload(add(ptr, 0x8)), 0xffffffffffffffffff)
+                let varLengthByte := shr(0x40, maxData)
+                newPtr := add(ptr, 1)
+                switch lt(varLengthByte, 0xfd)
+                case 1 { newPtr := add(newPtr, varLengthByte) }
+                default {
+                    // 0xfd -> 8 * 6
+                    // 0xfe -> 8 * 4
+                    // 0xff -> 8 * 0
+                    // 8 * (8 - 2^(vlb - 0xfc)) = 64 - 8 * 2^(vlb - 0xfc)
+                    let size := shl(sub(varLengthByte, 0xfc), 1)
+                    newPtr := add(newPtr, size)
+                    let data := and(maxData, 0xffffffffffffffff)
+                    newPtr := add(shl(sub(0x40, shl(3, size)), data), newPtr)
+                }
+            }
+
+            // Start pointer 4 bytes after beginning, skipping version bytes
+            let ptr := sub(_coinbaseTx.offset, 0x1b)
+
+            // Read and jump over tx inputs
+            let count
+            ptr, count := getVarint(ptr)
+            for { let i := 0 } lt(i, count) { i := add(i, 1) } {
+                ptr := add(ptr, 36)
+                ptr := varintSkip(ptr)
+                ptr := add(ptr, 4)
+            }
+            // Find witness root commitment output
+            ptr, count := getVarint(ptr)
+            let scriptLen
+            for {} 1 {} {
+                ptr := add(ptr, 8)
+                ptr, scriptLen := getVarint(ptr)
+                // Check length and witness commitment header.
+                if iszero(or(sub(scriptLen, 0x26), sub(and(calldataload(add(ptr, 5)), 0xffffffffffff), 0x6a24aa21a9ed)))
+                {
+                    // Get witness root
+                    witnessRoot := calldataload(add(ptr, 37))
+                    break
+                }
+                ptr := add(ptr, scriptLen)
+            }
         }
     }
 
--- test/btc-relay/BTCUtils.t.sol
+++ test/btc-relay/BTCUtils.t.sol
@@ -25,6 +25,26 @@ contract BTCUtilsTest is Test {
         assertTrue(this.callIsValidMerkle(leRoot, txid3, 2, proof));
     }
 
+    function testSimpleGetWitnessRoot() public {
+        assertEq(
+            this.callGetWitnessRootFromCoinbase(
+                hex"02000000010000000000000000000000000000000000000000000000000000000000000000ffffffff04028d4600ffffffff02786301000000000016001456f46329275352fb0b9c5d95c317d4e5b9d6a2860000000000000000266a24aa21a9ed024c152f229fe6400aeccb868252d04fdfb2a065ffadf39f7083ee533ceab47e00000000"
+            ),
+            bytes32(0x024c152f229fe6400aeccb868252d04fdfb2a065ffadf39f7083ee533ceab47e)
+        );
+    }
+
+    function testSimpleGetWitnessScript() public {
+        assertEq(
+            this.callGetWitnessScript(
+                hex"020000000001010f5b56b539c2dc11f49c357825cec2ba493217a1479198ae07aabeaa60ba08a40000000000fdffffff01282300000000000016001456f46329275352fb0b9c5d95c317d4e5b9d6a286044093211166ca954b9c2f9147712e93ed3f2f7e71e0e6c88bf2f703ab412353ebf301ad486f8acaa4deff485bc5c2acb4efe0de49636abc7db92531b03b32eb132920107661134f21fc7c02223d50ab9eb3600bc3ffc3712423a1e47bb1f9a9dbf55f45a8206c60f404f8167a38fc70eaf8aa17ac351023bef86bcb9d1086a19afe95bd533388204edfcf9dfe6c0b5c83d1ab3f78d1b39a46ebac6798e08e19761f5ed89ec83c10ac41c1f30544d6009c8d8d94f5d030b2e844b1a3ca036255161c479db1cca5b374dd1cc81451874bd9ebd4b6fd4bba1f84cdfb533c532365d22a0a702205ff658b17c900000000",
+                0
+            ),
+            hex"a8206c60f404f8167a38fc70eaf8aa17ac351023bef86bcb9d1086a19afe95bd533388204edfcf9dfe6c0b5c83d1ab3f78d1b39a46ebac6798e08e19761f5ed89ec83c10ac"
+        );
+    }
+
+    // Create external functions so that calldata can be tested
     function callIsValidMerkle(bytes32 _txRoot, bytes32 _baseEl, uint256 _index, bytes32[] calldata _proofEls)
         external
         view
@@ -39,6 +59,14 @@ contract BTCUtilsTest is Test {
         returns (bool)
     {
         return BTCUtils.isValidMerkleCoinbase(_txRoot, _baseEl, _proofEls);
+    }
+
+    function callGetWitnessScript(bytes calldata _tx, uint256 _inIndex) external pure returns (bytes memory) {
+        return BTCUtils.getWitnessScript(_tx, _inIndex);
+    }
+
+    function callGetWitnessRootFromCoinbase(bytes calldata _coinbaseTx) external returns (bytes32) {
+        return BTCUtils.getWitnessRootFromCoinbase(_coinbaseTx);
     }
 
     function sha256d(bytes memory _inp) internal pure returns (bytes32) {
